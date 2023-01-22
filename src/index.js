50fe2ac360f46e7776e3766f7fa68dbcd95e6a91
nosniklim <nosniklim_engineer@outlook.jp>
2023-01-22 12:14:24 +0000

    feat: マス目がクリックされた時にBoardクラスのstateを更新できるようにする

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
