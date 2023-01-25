a4fc5b271cc2c1ee32ff398bbf4af988b9bf0a00
nosniklim <nosniklim_engineer@outlook.jp>
2023-01-25 12:53:48 +0000

    fix: typo

--- src/index.js
+++ src/index.js
@@ -66,9 +66,11 @@ class Game extends React.Component {
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
@@ -111,7 +113,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={i => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
